0f693626d6b1589d51d6b9a31d04943d46a65c5a
danielvelasquez <danielrvelasquez@gmail.com>
2014-04-04 19:36:12 +0200

    InfoVis

---
+++ TMDbLib/Objects/Discover/DiscoverMovieSortBy.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+
+namespace TMDbLib.Objects.Discover
+{
+    //Enum for Sorting movies by attribute.
+    public enum DiscoverMovieSortBy
+    {
+        Undefined = 0,
+        [Description("vote_average.desc")]
+        VoteAverageDescending = 1,
+        [Description("vote_average.asc")]
+        VoteAverageAscending = 2,
+        [Description("release_date.desc")]
+        ReleaseDateDescending = 3,
+        [Description("release_date.asc")]
+        ReleaseDateAscending = 4,
+        [Description("popularity.desc")]
+        PopularityDescending = 5,
+        [Description("popularity.asc")]
+        PopularityAscending = 6
+    }
+}
---
+++ WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using TMDbLib.Client;
+using TMDbLib.Objects.Discover;
+using TMDbLib.Objects.General;
+using TMDbLib.Objects.Movies;
+using TMDbLib.Objects.Search;
+
+
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public static TMDbClient client = new TMDbClient("c83a7fa22254541c8eab822a78b7133c");
+        public ActionResult Index()
+        {
+            return View();
+        }
+        //Get changes to the timeline to update results.
+        [HttpPost]
+        public JsonResult SetTimeline(int min, int max)
+        {
+            SearchContainer<SearchMovie> result = new SearchContainer<SearchMovie>();
+            result.Results = new List<SearchMovie>();
+            result = client.DiscoverMovies(page: 1, sortBy: DiscoverMovieSortBy.PopularityDescending, releaseDateGreaterThan: new DateTime(min, 1, 1), releaseDateLessThan: new DateTime(max, 12, 31));
+
+            for (int cont = 2; cont <= 15 && cont <= result.TotalPages; cont++)
+            {
+                result.Results.InsertRange(0, client.DiscoverMovies(page: cont, sortBy: DiscoverMovieSortBy.PopularityDescending, releaseDateGreaterThan: new DateTime(min, 1, 1), releaseDateLessThan: new DateTime(max, 12, 31)).Results);
+            }
+            result.Results = result.Results.OrderBy(o => o.Popularity).ToList();
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
